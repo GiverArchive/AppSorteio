1439186b8dc17d0440ebdb3d0bacdeeff41294ca
GiverPlay007 <play.giver.007@gmail.com>
2021-11-14 18:42:24 -0300

    Projeto criado

---
+++ app/src/main/java/me/giverplay/appsorteio/MainActivity.java
@@ -0,0 +1,14 @@
+package me.giverplay.appsorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
