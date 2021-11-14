938c1a0abb20455202c42bbb680bc4c61c9bab6a
GiverPlay007 <play.giver.007@gmail.com>
2021-11-14 19:20:45 -0300

    Lógica da geração de números

--- app/src/main/java/me/giverplay/appsorteio/MainActivity.java
+++ app/src/main/java/me/giverplay/appsorteio/MainActivity.java
@@ -3,6 +3,10 @@ package me.giverplay.appsorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNumero(View view) {
+        TextView numero = findViewById(R.id.numero);
+        int x = new Random().nextInt(11);
+
+        numero.setText("Número: " + x);
     }
 }
